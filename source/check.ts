0eef5f1d47ddb75c79760f0260f9dd07bc52ac08
DaanV2 <daanverstraten@hotmail.com>
2021-10-19 14:52:41 +0200

    rebuild

--- source/check.ts
+++ source/check.ts
@@ -35,8 +35,8 @@ export class Checker {
           const commit_date = commit.commit.author.date;
 
           if (commit_date) {
-            console.log(`${commit.sha}=${commit_date}`);
             const commit_date_value = Date.parse(date);
+            console.log(`${commit.sha}=${commit_date_value}`);
 
             if (commit_date_value < date_value) {
               core.setOutput("amount", I);
